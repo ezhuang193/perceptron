ba3e158105404af924614906e105f088e9954b0b
Ben Homa <bhoma@rutgers.edu>
2016-08-14 01:40:14 -0400

    Update perceptronB.java

--- perceptronB.java
+++ perceptronB.java
@@ -13,78 +13,76 @@ public class Update {
 		int training_Size = 3;
 		int max_Iterations = 100;
 		// BufferedReader br = new BufferedReader(new FileReader("1.txt"));
-		 int[][] file = new int[20][30];
+		int[][] file = new int[20][30];
 		double label = 8;
 		double actual_output = 0;
 
-		// while(epoch < max_Iterations)
-		// {
-		for (int i = 1; i < training_Size; i++) {
-			String files = "train8/" + i + ".txt";
-			BufferedReader br = new BufferedReader(new FileReader(files));
-
-			// Reads File and saves in aray
-			for (int g = 0; g < 20; g++) {
-				for (int j = 0; j < 30; j++) {
-					file[g][j] = 0;
-				}
+		for (int dumb = 0; dumb < 2; dumb++) {
+			if (dumb == 1) {
+				training_Size = 130;
+				label = 3;
 			}
+			for (int i = 1; i < training_Size; i++) {
+				String files = "train8/" + i + ".txt";
 
-			for (int g = 0; g < 20; g++) {
-				String line = br.readLine();
-				int size = line.length();
+				if(dumb == 1) {
+					files = "trainOthers/" + i + ".txt";
+				}
 
-				for (int j = 0; j < size; j++) {
-					if (String.valueOf(line.charAt(j)).equals("+") || String.valueOf(line.charAt(j)).equals("#")) {
-						file[g][j] = 1;
-					} else
+				BufferedReader br = new BufferedReader(new FileReader(files));
+
+				// Reads File and saves in aray
+				for (int g = 0; g < 20; g++) {
+					for (int j = 0; j < 30; j++) {
 						file[g][j] = 0;
+					}
 				}
-			}
-			br.close();
 
-			int f = 0;
-			double sum = 0;
-			double output = 0;
-			for (int g = 0; g < 20; g++) {
-				for (int j = 0; j < 30; j++) {
-					weights[f] = file[g][j] * weights[f];
-					sum = sum + weights[f];
-					f++;
+				for (int g = 0; g < 20; g++) {
+					String line = br.readLine();
+					int size = line.length();
+
+					for (int j = 0; j < size; j++) {
+						if (String.valueOf(line.charAt(j)).equals("+") || String.valueOf(line.charAt(j)).equals("#")) {
+							file[g][j] = 1;
+						} else
+							file[g][j] = 0;
+					}
 				}
-			}
+				br.close();
 
-			if (sum > 0)
-				output = 1;
-			else
-				output = -1;
-
-			if (label == 8)
-				actual_output = 1;
-			else
-				actual_output = -1;
-
-			double err = actual_output - output;
-
-			f = 0;
-			for (int g = 0; g < 20; g++) {
-				for (int j = 0; j < 30; j++) {
-					weights[f] = weights[f] + (file[g][j] * learning_Rate * err);
-					f++;
+				int f = 0;
+				double sum = 0;
+				double output = 0;
+				for (int g = 0; g < 20; g++) {
+					for (int j = 0; j < 30; j++) {
+						weights[f] = file[g][j] * weights[f];
+						sum = sum + weights[f];
+						f++;
+					}
 				}
+
+				if (sum > 0)
+					output = 1;
+				else
+					output = -1;
+
+				if (label == 8)
+					actual_output = 1;
+				else
+					actual_output = -1;
+
+				double err = actual_output - output;
+
+				f = 0;
+				for (int g = 0; g < 20; g++) {
+					for (int j = 0; j < 30; j++) {
+						weights[f] = weights[f] + (file[g][j] * learning_Rate * err);
+						f++;
+					}
+				}
+
 			}
-
-			// System.out.println(f);
-			/*
-			 * for(int g=0; g<(height*width)+1; g++) {
-			 * 
-			 * }
-			 * 
-			 */
-
-		} // End of for
-		// epoch++;
-		// }//end of while
-
+		}
 	}
 }
